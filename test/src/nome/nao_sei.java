0d22f89026826f11cba1d673291025029139783d
Matthias Grob <matthias@paradis-guitars.com>
2020-10-10 23:42:40 +0200

    add nao sei

---
+++ test/src/nome/nao_sei.java
@@ -0,0 +1,15 @@
+package nome;
+
+public class nao_sei {
+	public static void main (String[] args) {
+	String[] nome = new String[5];
+	nome[0] = "icaue";
+	nome[1] = "icaue";
+	
+	int vida = 100;
+	if (vida >= 100) {
+		System.out.println("muitas vidas " + (3+4+5));
+	}
+	}
+}
+	
